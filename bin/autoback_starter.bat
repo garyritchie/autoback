d70abe8108a2ad88ec4c8d3c931f2a2f0dad9030
Gary Ritchie <gritchie@ibacos.com>
2013-10-16 09:20:05 -0400

    Update to starter script for Windows.

--- bin/autoback_starter.bat
+++ bin/autoback_starter.bat
@@ -2,9 +2,10 @@ REM ~ autoback_starter.bat
 
 REM ~ Use this from Windows Scheduler for daily backups. Can also be used to start a backup immediately.
 
+REM ~ Set current working directory
+cd /d Z:\BACKUP\autoback\bin
+
 REM ~ Fixes #1...
 set CYGWIN=nowinsymlinks 
-
-rem c:\cygwin\bin\sh autoback.sh
 
 c:\cygwin\bin\bash /cygdrive/z/BACKUP/autoback/bin/autoback.sh
